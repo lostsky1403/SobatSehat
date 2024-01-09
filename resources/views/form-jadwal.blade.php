d4a6159f0569e453f5f00e6d8c2ea22ccf5de8d7
lostsky1403 <moch22188ti@student.nurulfikri.ac.id>
2024-01-10 00:35:22 +0700

    perbaikan form kegiatan

--- resources/views/form-jadwal.blade.php
+++ resources/views/form-jadwal.blade.php
@@ -6,40 +6,36 @@
                 <div class="card bg-light text-black" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-justify">
                         <div class="mb-md-5 mt-md-4 pb-5">
-                            <form method="POST" action="{{ route('saveSchedule') }}">
-                                @csrf
-                                <div class="mb-3">
-                                    <label for="formGroupExampleInput" class="form-label">Lokasi Kegiatan</label>
-                                    <input type="text" class="form-control" id="formGroupExampleInput" name="location"
-                                        placeholder="">
-                                </div>
+                            <h2 class="fw-bold mb-2 text-uppercase">Buat Jadwalmu!</h2>
+                            <div class="mb-3">
+                                <label for="formGroupExampleInput" class="form-label">Lokasi Kegiatan</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="">
+                            </div>
 
-                                <div class="mb-3">
-                                    <label for="formGroupExampleInput2" class="form-label">Nama Kegiatan</label>
-                                    <input type="text" class="form-control" id="formGroupExampleInput2"
-                                        name="activity_name" placeholder="">
-                                </div>
+                            <div class="mb-3">
+                                <label for="formGroupExampleInput2" class="form-label">Nama Kegiatan</label>
+                                <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="">
+                            </div>
 
-                                <div class="mb-3">
-                                    <label for="formGroupExampleInput2" class="form-label">Tanggal Kegiatan</label>
-                                    <input type="datetime-local" class="form-control" id="tanggal" name="activity_date"
-                                        placeholder="">
-                                </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
-                                        name="send_email">
-                                    <label class="form-check-label" for="flexCheckDefault">
-                                        Kirim Jadwal ke Emailmu
-                                    </label>
-                                </div>
+                            <div class="mb-3">
+                                <label for="formGroupExampleInput2" class="form-label">Tanggal Kegiatan</label>
+                                <input type="datetime-local" class="form-control" id="tanggal" placeholder="">
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                <label class="form-check-label" for="flexCheckDefault">
+                                    Kirim Jadwal ke Emailmu
+                                </label>
+                            </div>
 
-                                <div class="form align-items-center">
-                                    <button class="btn btn-primary btn-lg px-5 py-10" type="submit">Buat Jadwalmu</button>
-                                </div>
-                            </form>
+                            <div class="form align-items-center">
+                                <a href="{{ url('/sukses') }}"><button class="btn btn-primary btn-lg px-5 py-10"
+                                        type="submit">Buat Jadwalmu</button></a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    @endsection
+    </div>
+@endsection
